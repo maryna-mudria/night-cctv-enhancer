3826aa554f11bcfa1f032e6a691e024e0c92a5c9
maryna-mudria <maryna.mudria@gmail.com>
2026-04-16 15:44:52 +0300

    num_workers=0 а то на винде мультипроцесс ругается

--- train.py
+++ train.py
@@ -27,8 +27,9 @@ val_size = int(len(full) * 0.1)
 train_size = len(full) - val_size
 train_ds, val_ds = random_split(full, [train_size, val_size])
 
-train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-val_loader = DataLoader(val_ds, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+# num_workers=0 потому что на винде ругается на мультипроцессинг без if __name__ == "__main__"
+train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+val_loader = DataLoader(val_ds, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
 # модель и оптимизатор
 model = UNet().to(DEVICE)
